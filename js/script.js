928f991335a968555d592ec4fd313ed0025efb29
İbrahim Mert Gül <ibrahimmertgul@gmail.com>
2020-04-30 05:10:37 +0300

    Exit butonu ve sweet alerti eklendi

--- js/script.js
+++ js/script.js
@@ -40,6 +40,22 @@ $("#exportBtn").click(function (e) {
 
 })
 
+$("#exitApp").click(function (e) {
+  Swal.fire({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, Exit!'
+  }).then((result) => {
+    if (result.value) {
+      ipcRenderer.send('exit');
+    }
+  }) 
+})
+
 // $("#test").click(function (e) {
 //   if (!isPreviewOn) {
 //     $("#editor").toggleClass("col-md-6");
--- main.js
+++ main.js
@@ -9,39 +9,41 @@ const Swal = require('sweetalert2')
 let win = [];
 app.on('ready', editorEkranı);
 
+
+
+ipcMain.on('exit', (ev)=>{
+    app.exit(0)
+});
+
 ipcMain.on('export', (ev, value)=>{
     //exportWindow()
     console.log(value);
 
 });
 
-
-
-function exportWindow(){
-    win[1] = new BrowserWindow({
-        width: 400,
-        height: 400,  
-        frame: false,
-        webPreferences: {
-            nodeIntegration: true,
-        }
-    })
-    win[1].loadURL(url.format({
-        pathname: path.join(__dirname, './pages/export.html'),
-        protocol: 'file:',
-        slashes: true
-    }))
-    // win[1].webContents.openDevTools();
-    win[1].on('closed', () => {
-        win[1] = null
-    })
-}
+// function exportWindow(){
+//     win[1] = new BrowserWindow({
+//         width: 400,
+//         height: 400,  
+//         frame: false,
+//         webPreferences: {
+//             nodeIntegration: true,
+//         }
+//     })
+//     win[1].loadURL(url.format({
+//         pathname: path.join(__dirname, './pages/export.html'),
+//         protocol: 'file:',
+//         slashes: true
+//     }))
+//     // win[1].webContents.openDevTools();
+//     win[1].on('closed', () => {
+//         win[1] = null
+//     })
+// }
 
 function editorEkranı() {
-    // Yeni bir pencere yarat
+
     win[0] = new BrowserWindow({
-        //width: 800,
-        //height: 600,
         fullscreen: true,
         frame: false,
         webPreferences: {
